4f4b5b9e9a7ba8cfb794b4c2f265417fa77df02c
Chuck Szeto <cszeto@learningobjects.com>
2014-06-17 18:08:08 -0400

    random commit to trigger github webhook

--- testling/test/max.js
+++ testling/test/max.js
@@ -3,7 +3,7 @@ var test = require('tape');
 
 test('simple comparisons', function (t) {
     t.plan(1);
-
+    //test
     var n = maxBy([9,3,4], function (x) { return x % 3 });
     t.equal(n, 4);
 });
