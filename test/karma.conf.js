580dc433fa57843aaea10803b045826868d88d46
Chuck Szeto <cszeto@learningobjects.com>
2014-06-17 18:04:35 -0400

    commenting out sauce labs for the time being, adding in testling sample code.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,42 +6,42 @@ var fs = require('fs');
 
 module.exports = function(config) {
 
-  // Use ENV vars on Travis and sauce.json locally to get credentials
-  if (!process.env.SAUCE_USERNAME) {
-    if (!fs.existsSync('sauce.json')) {
-      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
-      process.exit(1);
-    } else {
-      process.env.SAUCE_USERNAME = require('./sauce').username;
-      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
-    }
-  }
-  // Example set of browsers to run on Sauce Labs
-  // Check out https://saucelabs.com/platforms for all browser/platform combos
-  var customLaunchers = {
-    sl_chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7'
-    },
-    sl_firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '27'
-    },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
-  };
+  // // Use ENV vars on Travis and sauce.json locally to get credentials
+  // if (!process.env.SAUCE_USERNAME) {
+  //   if (!fs.existsSync('sauce.json')) {
+  //     console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+  //     process.exit(1);
+  //   } else {
+  //     process.env.SAUCE_USERNAME = require('./sauce').username;
+  //     process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+  //   }
+  // }
+  // // Example set of browsers to run on Sauce Labs
+  // // Check out https://saucelabs.com/platforms for all browser/platform combos
+  // var customLaunchers = {
+  //   sl_chrome: {
+  //     base: 'SauceLabs',
+  //     browserName: 'chrome',
+  //     platform: 'Windows 7'
+  //   },
+  //   sl_firefox: {
+  //     base: 'SauceLabs',
+  //     browserName: 'firefox',
+  //     version: '27'
+  //   },
+  //   sl_ios_safari: {
+  //     base: 'SauceLabs',
+  //     browserName: 'iphone',
+  //     platform: 'OS X 10.9',
+  //     version: '7.1'
+  //   },
+  //   sl_ie_11: {
+  //     base: 'SauceLabs',
+  //     browserName: 'internet explorer',
+  //     platform: 'Windows 8.1',
+  //     version: '11'
+  //   }
+  // };
 
 
 
@@ -102,7 +102,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
@@ -111,13 +111,13 @@ module.exports = function(config) {
     // URL root prevent conflicts with the site root
     // urlRoot: '_karma_'
 
-    sauceLabs: {
-        testName: 'Web App Unit Tests'
-    },
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers),
-    reporters: ['dots', 'saucelabs'],
-    singleRun: true
+    // sauceLabs: {
+    //     testName: 'Web App Unit Tests'
+    // },
+    // customLaunchers: customLaunchers,
+    // browsers: Object.keys(customLaunchers),
+    // reporters: ['dots', 'saucelabs'],
+    // singleRun: true
 
   });
 };
