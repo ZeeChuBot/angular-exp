da59bceeb390605133bd9c64962880c4e6b920f3
Chuck Szeto <cszeto@learningobjects.com>
2014-07-16 01:01:33 -0400

    removing testling

--- testling/example/max.js
+++
@@ -1,3 +0,0 @@
-var maxBy = require('../');
-var n = maxBy([9,3,4], function (x) { return x % 3 });
-console.log(n);
--- testling/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (xs, f) {
-    return xs.reduce(function (max, x) {
-        return f(x) > f(max) ? x : max;
-    });
-};
